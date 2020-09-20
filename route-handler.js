e2363be5592d4415dc3d18c2ccf0cc8b23014c7d
Sharvan Jain <sharjain@in.ibm.com>
2020-09-20 22:52:05 +0530

    adding the meta data

--- route-handler.js
+++ route-handler.js
@@ -3,11 +3,24 @@ let dataHandler = require('./data-handler')
 
 let getEvents = (req, res) => {
     let page = parseInt(req.query.page)
+    let events = {}
     if (!page || page === 0) {
-        res.send(dataHandler.generateEventsData(10))
+        events.page = 0
+        events.total = 115
+        events.totalPages =  12  
+        events.data = dataHandler.generateEventsData(10)
+    } else if (page >= 1 && page <=12) {
+        events.page = page
+        events.total = 115
+        events.totalPages =  12  
+        events.data = dataHandler.generateEventsData(page)
     } else {
-        res.send(dataHandler.generateEventsData(page))
+        events.page = page
+        events.total = 115
+        events.totalPages =  12  
+        events.data = []
     }
+    return events
 }
 
 module.exports = {
